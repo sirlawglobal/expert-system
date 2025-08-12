e88f5211c7d570e7cc43b2e18580cb058427a523
sirlaw <akanjilawrence9999@gmail.com>
2025-08-12 13:14:10 +0100

    sws
    swww

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const supportRoute = require('./routes/support');
 app.use(cors());
 app.use(express.json());
 app.use('/api/support', supportRoute);
+app.get('/', (req, res) => {
+  res.send('Expert System Backend is running on Render 🚀');
+});
 
 const PORT = 4000;
 app.listen(PORT, () => {
